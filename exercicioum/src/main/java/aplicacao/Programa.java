0191a9f0c7ead3b8f6aa56b94d518e98a6e1a627
Rafaella2403 <rribeirolimabritto@gmail.com>
2022-01-19 00:39:45 -0300

    Buscando de deletando dados do banco

--- exercicioum/src/main/java/aplicacao/Programa.java
+++ exercicioum/src/main/java/aplicacao/Programa.java
@@ -10,14 +10,16 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
+		/* Comentando o bloco para não inserir novamente os mesmos dados
 		Pessoas pessoa1 = new Pessoas(null, "Rafaella Ribeiro Lima Britto", "rribeirolimabritto@gmail.com");
 		Pessoas pessoa2 = new Pessoas(null, "Wallyson Richad Gomes da Silva", "wallyson@gmail.com");
-		Pessoas pessoa3 = new Pessoas(null, "Flavio Lima Britto", "flavio@gmail.com");
+		Pessoas pessoa3 = new Pessoas(null, "Flavio Lima Britto", "flavio@gmail.com");*/
 		
 		//Fazendo a conexão com o banco de dados
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("dados-pessoas");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		
+		/* Comentando o bloco para não inserir novamente os mesmos dados
 		//Quando o JPA faz alguma operação que não é consulta no banco ele necessita de uma transação.
 		entityManager.getTransaction().begin();
 		
@@ -27,7 +29,21 @@ public class Programa {
 		entityManager.persist(pessoa3);
 	
 		//Confirmando as alterações realizadas
+		entityManager.getTransaction().commit();*/
+		
+		//Buscando dados do banco pelo Id
+		Pessoas pessoa = entityManager.find(Pessoas.class, 2);
+		System.out.println(pessoa);
+		
+		//Apagando um cadastro do banco de dados pelo id
+		entityManager.getTransaction().begin();
+		entityManager.remove(pessoa);
 		entityManager.getTransaction().commit();
+		
+		
+		//Fechando a conexão
+		entityManager.close();
+		entityManagerFactory.close();
 	}
 
 }
