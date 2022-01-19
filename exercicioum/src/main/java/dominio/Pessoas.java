43427ebc267c9acd7946b14b090e01d2912d5b40
Rafaella2403 <rribeirolimabritto@gmail.com>
2022-01-19 00:05:52 -0300

    Configurando o JPA no projeto Maven

---
+++ exercicioum/src/main/java/dominio/Pessoas.java
@@ -0,0 +1,62 @@
+package dominio;
+
+import java.io.Serializable;
+
+public class Pessoas implements Serializable{
+	
+	/*Descrição da atividade
+	 * Faça as configurações da classe Pessoa e os mapeamentos*/
+	
+	private Integer id;
+	private String nome;
+	private String email;
+	private static final long serialVersionUID = 1L;
+
+	public Pessoas() {
+		
+	}
+	
+	public Pessoas(Integer id, String nome, String email) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Pessoas [id: "
+				+ id 
+				+ ", nome: " 
+				+ nome 
+				+ ", email: " 
+				+ email 
+				+ "]";
+	}
+	
+	
+}
